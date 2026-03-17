4275de557a7810970854f2c66b66a127a7d3f0ae
Kashish Gupta <gkashish864@gmail.com>
2026-03-17 06:55:43 +0530

    Add Semester life simulation in Semester_life.cpp

---
+++ Semester_life.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void semseter_ki_jai (){
+    cout<<"Day one orientation\n";
+    cout<<"Mid semester chalu ho gaye\n";
+    cout<<"End sem ki date sheet aa gayi\n";
+    cout<<"End sem ki practical date sheet aa gayi\n";
+    cout<<"1 semester khatam";
+}
+
+int main(){
+    semseter_ki_jai();
+    semseter_ki_jai();
+    semseter_ki_jai();
+    semseter_ki_jai();
+    return 0;
+
+}
