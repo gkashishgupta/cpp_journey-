f590d423d934a73f82842e1ffde7424f6a39ccb5
Kashish Gupta <gkashish864@gmail.com>
2026-03-18 18:27:23 +0530

    Add btech_life.cpp to describe college semesters

---
+++ btech_life.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for (int i= 1;i<=8;i++){
+        cout << "Semester " << i << ": Orientation, sab naya naya" << endl;
+        cout<<  "Semester " << i << ":C++ aur M2 se takkar"<<endl;
+        cout<<  "Semester " << i << ":DSA ka darr shuru"<<endl;
+        cout<<  "Semester " << i << ":Projects banana start"<<endl;
+        cout<<  "Semester " << i << ":Internship ki tension"<<endl;
+        cout<<  "Semester " << i << ":Internship mil gayi ya nahi "<<endl;
+        cout<<  "Semester " << i << ":Placements ka bhoot"<<endl;
+        cout<<  "Semester " << i << ":Placement ho gayi - Google mai"<<endl;
+        cout<<  "Semester " << i << ":uth ja bhai tu abhi 1sem toh nikal le"<<endl;
+        
+    }
+}
+
+
