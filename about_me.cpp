867d83bc78275d670306ba466aac8b4b05142265
Kashish Gupta <gkashish864@gmail.com>
2026-03-19 23:07:41 +0530

    Create about_me.cpp

---
+++ about_me.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string name = "Kashish";
+    int year = 2026;
+    
+    cout<<"Name: "<<name<<endl;
+    cout<<"Started coding in: "<<year<<endl;
+    cout<<"Goal: process mai hai but ''MNC'' join karna hai"<<endl;
+    cout<<"Hobbies:Claude ko suna na"<<endl;
+    cout<<"Status: Still going "<<endl;
+    
+    return 0;
+}
