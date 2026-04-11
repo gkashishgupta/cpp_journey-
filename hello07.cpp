5c492affc693b3a658ec3ce8ae0314c120de7dcd
Kashish <gkashish864@gmail.com>
2026-04-11 06:09:05 +0530

    radius program

---
+++ hello07.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   const double PI = 3.1419;
+   double radius = 5;
+   double circumference = 2 * PI * radius;
+
+   std::cout<<circumference<<" cm";
+
+    return 0;
+}
---
+++ practice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   const double PI = 3.14159;
+   double radius;
+   
+   cout << "Radius enter karo: ";
+   cin >> radius;
+   
+   double circumference = 2 * PI * radius;
+   cout << circumference << " cm";
+
+   return 0;
+}
